bedb073c4593bc248c7e53a1ad9b31e539f00cc1
acsm5 <132668755+acsm5@users.noreply.github.com>
2023-08-25 11:57:54 +0000

    ..

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-import express from "express";      
-const app = express();              
-const port = 3000;                 
+//index.js
+import dotenv from "dotenv";
+import express from "express";
+import { selectUsuarios } from "./bd.js";
 
-app.get("/", (req, res) => {        
-  console.log("Rota GET/ solicitada");
+dotenv.config();
+
+const app = express(); 
+const port = 3000; 
+
+app.get("/", (req, res) => {
+  console.log("Rota / solicitada");
+  
   res.json({
-    nome: "Ayala Clara Silva Miranda",      
+    nome: "Ayala Clara Silva Miranda",
   });
 });
 
-app.listen(port, () => {            
+app.get("/usuarios", async (req, res) => {
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+
+  console.log("Rota GET/usuarios solicitada");
+});
+
+app.listen(port, () => {
+  
   console.log(`Serviço escutando na porta:  ${port}`);
 });
