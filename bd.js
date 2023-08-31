ac8594022c044fac37aeea38be73e393702a5476
acsm5 <132668755+acsm5@users.noreply.github.com>
2023-08-31 12:52:07 +0000

    kljg

--- bd.js
+++ bd.js
@@ -13,6 +13,13 @@ async function selectUsuarios() {
   const client = await connect();
   const res = await client.query("SELECT * FROM usuario");
   return res.rows;
-}
 
-export { selectUsuarios };
+  //bd.js
+async function selectUsuario(id) {
+  const client = await connect();
+  const query = "SELECT * FROM usuario WHERE id = $1";
+  const usuario = [id];
+  const res = await client.query(query, usuario);
+  return res.rows;
+}
+export { selectUsuarios, selectUsuario };
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 //index.js
 import dotenv from "dotenv";
 import express from "express";
-import { selectUsuarios } from "./bd.js";
-
+import { selectUsuarios, selectUsuario } from "./bd.js";
 dotenv.config();
 
 const app = express(); 
@@ -15,15 +14,16 @@ app.get("/", (req, res) => {
     nome: "Ayala Clara Silva Miranda",
   });
 });
-
-
-app.get("/usuarios", async (req, res) => {
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario solicitada");
   try {
-    const usuarios = await selectUsuarios();
-    res.json(usuarios);
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
+});
 
   console.log("Rota GET/usuarios solicitada");
 });
@@ -32,3 +32,5 @@ app.listen(port, () => {
   
   console.log(`Serviço escutando na porta:  ${port}`);
 });
+
+
