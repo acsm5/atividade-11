b6e245e09b1ee89582960fff1a102732a5034780
acsm5 <132668755+acsm5@users.noreply.github.com>
2023-08-31 13:49:22 +0000

    kfkj

--- bd.js
+++ bd.js
@@ -8,13 +8,13 @@ async function connect() {
   });
   return pool.connect();
 }
-
+//bd.js
 async function selectUsuarios() {
   const client = await connect();
   const res = await client.query("SELECT * FROM usuario");
   return res.rows;
+}
 
-  //bd.js
 async function selectUsuario(id) {
   const client = await connect();
   const query = "SELECT * FROM usuario WHERE id = $1";
@@ -22,4 +22,12 @@ async function selectUsuario(id) {
   const res = await client.query(query, usuario);
   return res.rows;
 }
-export { selectUsuarios, selectUsuario };
+async function insertUsuario(data) {
+  const client = await connect();
+  const query = "INSERT INTO usuario (nome,senha,email) VALUES ($1,$2,$3) ";
+  const usuario = [data.nome, data.senha, data.email];
+  await client.query(query, usuario);
+}
+
+export { selectUsuarios, selectUsuario, insertUsuario };
+
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //index.js
 import dotenv from "dotenv";
 import express from "express";
-import { selectUsuarios, selectUsuario } from "./bd.js";
+import { selectUsuarios, selectUsuario, insertUsuario } from "./bd.js";
 dotenv.config();
 
 const app = express(); 
@@ -14,23 +14,33 @@ app.get("/", (req, res) => {
     nome: "Ayala Clara Silva Miranda",
   });
 });
-app.get("/usuario/:id", async (req, res) => {
-  console.log("Rota GET /usuario solicitada");
-  try {
-    const usuario = await selectUsuario(req.params.id);
-    if (usuario.length > 0) res.json(usuario);
-    else res.status(404).json({ message: "Usuário não encontrado!" });
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  app.get("/usuario/:id", async (req, res) => {
+    console.log("Rota GET /usuario solicitada");
+    try {
+      const usuario = await selectUsuario(req.params.id);
+      if (usuario.length > 0) res.json(usuario);
+      else res.status(404).json({ message: "Usuário não encontrado!" });
+     }catch (error) {
+      res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
 });
 
-  console.log("Rota GET/usuarios solicitada");
-});
+    console.log("Rota GET/usuarios solicitada");
+
 
 app.listen(port, () => {
   
   console.log(`Serviço escutando na porta:  ${port}`);
 });
 
+app.use(express.json());
 
+app.post("/usuario", async (req, res) => {
+  console.log("Rota POST /usuario solicitada");
+  try {
+    await insertUsuario(req.body);
+    res.status(201).json({ message: "Usuário inserido com sucesso!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
